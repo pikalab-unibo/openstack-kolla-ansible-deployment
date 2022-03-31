ffce1115b921056e8e956454155cbc5d5105fa03
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2022-03-31 19:10:15 +0200

    fix: globals

--- home/install-openstack.sh
+++ home/install-openstack.sh
@@ -1,3 +1,11 @@
+ansible -i test.ini all -m command -a "yum install -y yum-utils" -e "@secrets.yml"
+ansible -i test.ini all -m command -a "yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo" -e "@secrets.yml"
+ansible -i test.ini all -m command -a "yum install --allowerasing -y docker-ce docker-ce-cli containerd.io" -e "@secrets.yml"
+ansible -i test.ini all -m command -a "systemctl start docker" -e "@secrets.yml"
+ansible -i test.ini all -m command -a "systemctl enable docker" -e "@secrets.yml"
+
+# No host NTP daemon is running, and the Kolla Ansible chrony container is disabled. Please install and configure a host NTP daemon. Alternatively, set 'prechecks_enable_host_ntp_checks' to 'false' to disable this check if not using one of the following NTP daemons: chrony, ntpd, systemd-timesyncd.
+
 echo "### Bootstrapping servers #####################################################"
 kolla-ansible -i ./multinode.ini bootstrap-servers -e "@secrets.yml"
 echo
