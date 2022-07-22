1bd43365b063b85419b010cb352ffef1db66164a
gloriasemprini <99739306+gloriasemprini@users.noreply.github.com>
2022-07-22 17:01:03 +0200

    Update install-openstack.sh
    Aggiunta l'installazione effettiva di openstack client e post-deploy

--- home/install-openstack.sh
+++ home/install-openstack.sh
@@ -8,4 +8,11 @@ kolla-ansible -i ./multinode.ini prechecks -e "@secrets.yml"
 echo
 echo "### Depoly ####################################################################"
 kolla-ansible -i ./multinode.ini deploy -e "@secrets.yml"
+echo
+echo "### Install Openstack Client ######################################################################"
+pip install python-openstackclient -c https://releases.openstack.org/constraints/upper/xena
+echo
+echo "### Post-Deploy ######################################################################"
+kolla-ansible post-deploy -e "@secrets.yml"
+. /etc/kolla/admin-openrc.sh
 echo "### Done ######################################################################"
